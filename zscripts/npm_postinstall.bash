e5050d5dd7da4815cd440d1dfbf7eff4915ad1c4
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-02-13 23:17:55 +1000

    Fix script for codebuild

--- zscripts/npm_postinstall.bash
+++ zscripts/npm_postinstall.bash
@@ -2,9 +2,11 @@
 
 set -e
 
-echo ">>> copy git pre-commit hook <<<"
-cp ./zscripts/git-pre-commit.sh ./.git/hooks/pre-commit
-chmod +x ./.git/hooks/pre-commit
+if [[ -z $CODEBUILD_START_TIME ]]; then
+    echo ">>> copy git pre-commit hook <<<"
+    cp ./zscripts/git-pre-commit.sh ./.git/hooks/pre-commit
+    chmod +x ./.git/hooks/pre-commit
+fi
 
 if [[ -d "./node_modules/@cpmech/rcomps/rcomps" ]]; then
     echo ">>> copy rcomps to src <<<"
